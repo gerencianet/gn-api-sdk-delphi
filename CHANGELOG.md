5a75ddeb6e2fd1eb011abbe86e50dc234a41107d
Whinter Goncalves <whinter.goncalves@gerencianet.com.br>
2019-08-28 14:31:35 -0300

    change doc version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+# 3.1.0
+
+- Add: New endpoint (oneStep)
+
+# 3.0.0
+
+- Add: TLS 1.2 support
+
 # 2.0.0
 
 - Add: Proxy configurations and New endpoints (settleCharge and settleCarnetParcel)
--- src/dll-src/uGerenciaNetIndy.pas
+++ src/dll-src/uGerenciaNetIndy.pas
@@ -18,7 +18,7 @@ threadvar ClientID, ClientSecret, Environment, ConfigFileName,
           ProxyPort: Integer;
           ExpireDateTime : TDateTime;
 
-const API_SDK = 'delphi-3.0.2';
+const API_SDK = 'delphi-3.1.0';
 
 implementation
 
